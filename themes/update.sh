bce80af02f08da139e6746f6263f2d4fe777b1a1
Takayama Fumihiko <tekezo@pqrs.org>
2019-09-16 23:41:50 +0900

    add themes

---
+++ themes/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u
+
+basedir=$(dirname $0)
+cd $basedir
+pwd=$(pwd)
+
+rm -fr docsy
+git clone --recursive --shallow-submodules --depth 1 https://github.com/google/docsy.git
+
+rm -f docsy-revisions
+for d in $(find docsy -name '.git'); do
+    echo -n "$d: " >> docsy-revisions
+    (cd $(dirname $d) && git rev-parse HEAD) >> docsy-revisions
+done
+
+for d in $(find docsy -name '.git'); do
+    rm -fr $d
+done
