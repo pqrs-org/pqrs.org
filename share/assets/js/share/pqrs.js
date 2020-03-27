4f1fe43a8e84b1d0a39b536319f9a2d524f522fc
Takayama Fumihiko <tekezo@pqrs.org>
2020-03-27 22:33:39 +0900

    skip zip,gz,dmg from setting external link target

--- share/assets/js/share/pqrs.js
+++ share/assets/js/share/pqrs.js
@@ -5,6 +5,14 @@ $(function() {
     const $this = $(this)
     const href = $this.attr('href')
     if (href.startsWith('http://') || href.startsWith('https://')) {
+      if (
+        href.endsWith('.zip') ||
+        href.endsWith('.gz') ||
+        href.endsWith('.dmg')
+      ) {
+        return
+      }
+
       $this.attr('target', '_blank')
       $this.append($('<span>').addClass('fas fa-external-link-alt ml-1'))
     }
