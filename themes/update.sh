8f84dc79b4eaa4af9d1614d815fcc9d0b799a467
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-07 21:55:24 +0900

    Updatge docsy

--- themes/update.sh
+++ themes/update.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-set -u
+set -u # forbid undefined variables
+set -e # forbid command failure
+
+docsyrev=v0.2.0
 
 basedir=$(dirname $0)
 cd $basedir
 pwd=$(pwd)
 
 rm -fr docsy
-git clone --recursive --shallow-submodules --depth 1 https://github.com/google/docsy.git
+git clone \
+  --recursive \
+  --shallow-submodules \
+  --depth 1 \
+  --branch $docsyrev \
+  https://github.com/google/docsy.git
 
 rm -f docsy-revisions
 for d in $(find docsy -name '.git'); do
-    echo -n "$d: " >> docsy-revisions
-    (cd $(dirname $d) && git rev-parse HEAD) >> docsy-revisions
+  echo -n "$d: " >>docsy-revisions
+  (cd $(dirname $d) && git rev-parse HEAD) >>docsy-revisions
 done
 
 for d in $(find docsy -name '.git'); do
-    rm -fr $d
+  rm -fr $d
 done
 
 rm -fr docsy/userguide
