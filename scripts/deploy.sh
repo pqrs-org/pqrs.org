c6b5736075f5cc7ef4e9f53d95577bbf6c26c1ff
Takayama Fumihiko <tekezo@pqrs.org>
2024-09-11 00:08:43 +0900

    Update deploy.sh (gh-pages-* -> sites-*)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,46 +19,46 @@ function deploy() {
     exit 1
   fi
 
-  rsync -a --delete --exclude CNAME --exclude .nojekyll $source $repo/docs
+  rsync -a --delete $source $repo/public
   (cd $repo && git status -s)
 }
 
 deploy \
-  ../gh-pages-bulletgba.pqrs.org \
+  ../sites-bulletgba.pqrs.org \
   sites/bulletgba/public/
 
 deploy \
-  ../gh-pages-combostick.pqrs.org \
+  ../sites-combostick.pqrs.org \
   sites/combostick/public/
 
 deploy \
-  ../gh-pages-dcgnuboy.pqrs.org \
+  ../sites-dcgnuboy.pqrs.org \
   sites/dcgnuboy/public/
 
 deploy \
-  ../gh-pages-disable-draganddrop-tb.pqrs.org \
+  ../sites-disable-draganddrop-tb.pqrs.org \
   sites/disable-draganddrop-tb/public/
 
 deploy \
-  ../gh-pages-karabiner-elements.pqrs.org \
+  ../sites-karabiner-elements.pqrs.org \
   sites/karabiner-elements/public/
 
 deploy \
-  ../gh-pages-nesterdc.pqrs.org \
+  ../sites-nesterdc.pqrs.org \
   sites/nesterdc/public/
 
 deploy \
-  ../gh-pages-showyedge.pqrs.org \
+  ../sites-showyedge.pqrs.org \
   sites/showyedge/public/
 
 deploy \
-  ../gh-pages-tinkle.pqrs.org \
+  ../sites-tinkle.pqrs.org \
   sites/tinkle/public/
 
 deploy \
-  ../gh-pages-truewidget.pqrs.org \
+  ../sites-truewidget.pqrs.org \
   sites/truewidget/public/
 
 deploy \
-  ../gh-pages-vulkanon.pqrs.org \
+  ../sites-vulkanon.pqrs.org \
   sites/vulkanon/public/
